e03d438fa4e6a391edf59fd2c4bcaccc7334c333
bozo <luka.bozanovic2@gmail.com>
2025-12-09 11:59:44 +0100

    kalkulator

---
+++ kalkulator/index.js
@@ -0,0 +1,24 @@
+let PrviBroj=document.getElementById("PrviUnos");
+let DrugiBroj=document.getElementById("DrugiUnos");
+
+function Zbroj(){
+    
+}
+
+function Kolicnik(){
+
+}
+
+function Umnozak(){
+
+}
+
+function Razlika(){
+
+}
+
+
+
+function newCalculate(){
+    input.value="";
+}
