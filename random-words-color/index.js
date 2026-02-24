185d7fb39a9166b1a4cfd9139b39ce8806e3592c
LukaBozo <luka.bozanovic2@gmail.com>
2026-02-24 15:26:28 +0100

    dns

---
+++ random-words-color/index.js
@@ -0,0 +1,7 @@
+import randomColor from "randomcolor";
+import names from "starwars-names";
+
+var color = randomColor();
+var randomName = names.random();
+console.log(`${color} i ${randomName}`);
+
